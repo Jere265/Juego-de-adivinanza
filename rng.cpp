e785e73f75c9b6aa1eb866cec517d85439a23e2e
Jere265 <66541165+Jere265@users.noreply.github.com>
2023-06-25 21:18:59 -0300

    empiezo con el juego
    deja adivinar solo una vez

--- rng.cpp
+++ rng.cpp
@@ -6,7 +6,23 @@
 
 using namespace std;
 
+/*
 int generar_numero() {
+	
+	9876/ 
+	
+	return numero;
+}
+*/
+
+int main (){
+
+	int intento,e,f,g,h;
+	int primerdigito = 0;
+	int segundodigito = 0;
+	int tercerdigito = 0;
+	int cuartodigito = 0;
+	int posicionescorrectas = 0;
 	
 	srand(time(0));
 	int a = rand() % 10;
@@ -21,23 +37,55 @@ int generar_numero() {
 	int d = rand() % 10;
 	
 	int numero = a*1000+b*100+c*10+d;
+
+	cout << "numero = " << numero << endl;
 	
-	return numero;
-}
-
-int main (){
+	cin >> intento;
+	cout << "intento = " << intento << endl;
 	
-
-//int a, b, c, d;
-
-
-
-cout << "a = " << generar_numero() << endl;
-cout << "b = " << generar_numero() << endl;
-cout << "c = " << generar_numero() << endl;
-cout << "d = " << generar_numero() << endl;
-
-return 0;
+	h = intento % 10;
+	intento = intento / 10;
+	
+	g = intento % 10;
+	intento = intento / 10;
+	
+	f = intento % 10;
+	intento = intento / 10;
+	
+	e = intento % 10;
+	intento = intento / 10;
+	
+	cout << "e = " << e << endl;
+	cout << "f = " << f << endl;
+	cout << "g = " << g << endl;
+	cout << "h = " << h << endl;
+	
+	if (a == e){
+		primerdigito = 1;
+	}
+	
+	if (b == f){
+		segundodigito = 1;
+	}
+	
+	if (c == g){
+		tercerdigito = 1;
+	}
+	
+	if (d == h){
+		cuartodigito = 1;
+	}
+	
+	cout << "primer digito = " << primerdigito << endl;
+	cout << "segundo digito = " << segundodigito << endl;
+	cout << "tercer digito = " << tercerdigito << endl;
+	cout << "cuarto digito = " << cuartodigito << endl;
+	
+	posicionescorrectas = primerdigito + segundodigito + tercerdigito + cuartodigito;
+	
+	cout << "Posiciones correctas = " << posicionescorrectas << endl;
+	
+	return 0;
 }
 
 
